4420b34464c302facd431c44236f60d01ac05958
HoangLe87 <hongacek@hotmail.com>
2022-12-04 08:53:24 +0100

    FE and BE deployed

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,12 +1,60 @@
-import * as React from "react";
-import { ethers } from "ethers";
+import React, { useEffect, useState } from "react";
+
+const getEthereumObject = () => window.ethereum;
+
+
+const findMetaMaskAccount = async() => {
+  try {
+    const ethereum = getEthereumObject()
+    if (!ethereum) {
+      console.log('Please install metamask')
+      return null
+    }
+    console.log(`We have etherium object ${ethereum}`)
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account:", account);
+      return account;
+    } else {
+      console.error("No authorized account found");
+      return null;
+    }
+  } catch(error) {
+    console.log(error)
+    return null;
+  }
+}
+
 
 export default function App() {
+  const [currentAccount, setCurrentAccount] = useState("");
 
-  const wave = () => {
-    
+  const connectWallet = async() => {
+    try {
+      const ethereum = getEthereumObject()
+      if (!ethereum) {
+        alert("get metamask")
+        return
+      }
+      const accounts = ethereum.request({method: "eth_requestAccounts"});
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch(error) {
+      console.log(error)
+    }
   }
-  
+
+  useEffect(()=> {
+    const getAccount = async() => {
+      const account = await findMetaMaskAccount()
+      if (!account!=null) {
+        setCurrentAccount(account)
+      }
+    }
+    getAccount()
+  }, [])
+
   return (
     <div className="mainContainer">
 
@@ -16,12 +64,20 @@ export default function App() {
         </div>
 
         <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+        I am Hoang, besides chocolate and other sweetness goodies
+        <br/>I really love a good wave.
+        <br/>Connect your Ethereum wallet and wave at me!
         </div>
-
-        <button className="waveButton" onClick={wave}>
+        <div className="waveButton">
+        <button onClick={null}>
           Wave at Me
         </button>
+        {!currentAccount && (
+          <button className="waveButton" onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        )}
+        </div>
       </div>
     </div>
   );
--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 Simple project using:
     - solidity deployed to testnet
     - vite for front end
+
+Info:
+Deploying contracts with account 0x4cEE6B545906e927Ea1f9f2f271f7db7e41328D9
+Account balance 200000000000000000
+contract deployed to 0x369854AdB5E37E39D1539710f75AedaA0D3597B4
